0da80f155d53f90e008f1588e57173ed63721a6f
Marco Wang <marcowang01@gmail.com>
2023-12-22 16:10:37 +0900

    insert db script works

--- scripts/initialize_db.ts
+++ scripts/initialize_db.ts
@@ -5,11 +5,13 @@ import fs from 'fs';
 import { config } from 'dotenv';
 config({ path: './.env.local' }); // load in .env.local explicitly
 
-const STYLE_COL = 1;
-const COLOR1_COL = 2; // Assuming color columns start from column 2
-const COLOR_COUNT = 7; // Number of color columns
+const STYLE_COL = 1; // column index of style name
+const COLOR1_COL = 2; // Assuming color columns start from column 2 (idx of col for 'color1')
+const COLOR_COUNT = 7; // Number of color columns (# of colors per pallete) assumes everything after are the color filters
+const NUM_FILTERS = 11; // Number of color filters
+const TOTAL_COLS = COLOR1_COL + COLOR_COUNT + NUM_FILTERS; // total number of columns in the csv
 
-
+// simple local cache to avoid unnecessary db calls
 class LocalCache {
   styles: { [key: string]: number } = {};
   filters: { [key: string]: number } = {};
@@ -65,7 +67,7 @@ export async function createTables() {
 // todo: write function to fetch and cache styles in the state/redis?
 // todo: make sure colors are ordered lexigraphically on insertion
 
-// helper function to fetch id for style or filter
+// helper function to fetch id for style or filter from the sql table
 // todo: maybe do better error handling here but fine for now
 async function fetchIdFromTable(type: 'filter' | 'style', value: string, cache: LocalCache): Promise<number> {
   const table = type + 's';
@@ -78,23 +80,43 @@ async function fetchIdFromTable(type: 'filter' | 'style', value: string, cache: 
   }
 
   // if not in cache, fetch from db
-  const res: QueryResult<QueryResultRow> = await sql`
-    SELECT ${id}
-    FROM ${table}
-    WHERE ${name} = ${value}
-  `;
-  
+  let res: QueryResult<QueryResultRow>
+
+  if (type === 'style') {
+    res = await sql`
+      SELECT style_id
+      FROM styles
+      WHERE style_name = ${value}
+    `;
+  } else {
+    res = await sql`
+      SELECT filter_id
+      FROM filters
+      WHERE filter_name = ${value}
+    `;
+  }
+
   // if entry exists, cache and return id
   if (res.rowCount > 0) {
     cache[table][value] = res.rows[0][id];
   } else {
     // otherwise, insert into table and cache
-    const res: QueryResult<QueryResultRow> = await sql`
-      INSERT INTO ${table} (${name})
-      VALUES (${value})
-      RETURNING ${id}
-    `;
-    cache[table][value] = res.rows[0][id];
+    let id_res: QueryResult<QueryResultRow>
+    if (type === 'style') {
+      id_res = await sql`
+        INSERT INTO styles (style_name)
+        VALUES (${value})
+        returning style_id
+      `;
+    } else {
+      id_res = await sql`
+        INSERT INTO filters (filter_name)
+        VALUES (${value})
+        returning filter_id
+      `;
+    }
+
+    cache[table][value] = id_res.rows[0][id];
   }
 
   return cache[table][value];
@@ -102,12 +124,14 @@ async function fetchIdFromTable(type: 'filter' | 'style', value: string, cache: 
 
 export async function populateTablesFromCSV(path: string) {
   try {
+    console.log("reading csv...")
     const palettesCSV = fs.readFileSync(path, 'utf8');
     // const styles: Set<string> = new Set();
     // const colors: Set<string> = new Set();
     const cache = new LocalCache();
     const rows = palettesCSV.split('\n');
 
+    console.log("parsing csv headers...")
     // Extract headers for color names
     const headers = rows.shift()?.split(','); // Remove headers
     if (!headers) {
@@ -116,26 +140,38 @@ export async function populateTablesFromCSV(path: string) {
 
     // get all filter names from palettes.csv by looking at the headers
     // hard coded to skip the first 9 columns
-    const filters = headers.slice(COLOR1_COL + COLOR_COUNT)
+    const filters = headers.slice(COLOR1_COL + COLOR_COUNT) // array storing all filter names
+
+    console.log("populating styles table...")
     for (const filter of filters) {
       const filter_name = filter.trim();
       // cache filter ids by "fetching" id
       await fetchIdFromTable('filter', filter_name, cache);
     }
 
+    console.log("populating palettes table...")
+    let count = 0;
     for (const row of rows) {
+      count++;
+      console.log("inserting row #" + count)
       const columns = row.split(',');
+      if (columns.length != TOTAL_COLS) {
+        console.log(`invalid # of cols. skipping row ${count}: ${row}`);
+        continue;
+      }
       const image = columns[0].trim();
       const style = columns[STYLE_COL].trim();
       const stlye_id = await fetchIdFromTable('style', style, cache); // cache style ids and populate styles table
       const colors = columns.slice(COLOR1_COL, COLOR1_COL + COLOR_COUNT).map(color => color.trim());
-      const filter_flags = columns.slice(COLOR1_COL + COLOR_COUNT).map(filter => filter.trim());
+      const filter_color_flags = columns.slice(COLOR1_COL + COLOR_COUNT).map(filter => filter.trim());
 
       // insert palette data
+      // need to gaurantee that the number of colors on the pallete is 7
       if (colors.length !== COLOR_COUNT) {
         console.log(`img: ${image}, style: ${style}, colors: ${colors}`)
         throw new Error('Error parsing CSV: invalid number of colors');
       }
+
       const res = await sql`
         INSERT INTO palettes (image, style_id, color1, color2, color3, color4, color5, color6, color7)
         VALUES (${image}, ${stlye_id}, ${colors[0]}, ${colors[1]}, ${colors[2]}, ${colors[3]}, ${colors[4]}, ${colors[5]}, ${colors[6]})
@@ -144,8 +180,8 @@ export async function populateTablesFromCSV(path: string) {
       const pallete_id = res.rows[0].palette_id;
     
       // populate filters table and palettes_filters table
-      for (const [index, filter_flag] of filter_flags.entries()) {
-        if (filter_flag === '1') {
+      for (const [index, flag] of filter_color_flags.entries()) {
+        if (flag === '1') {
           const filter_name = filters[index];
           const filter_id = await fetchIdFromTable('filter', filter_name, cache);
           await sql`
@@ -154,58 +190,6 @@ export async function populateTablesFromCSV(path: string) {
           `;
         }
       }
-
-
-
-    }
-
-    // Extract styles and prepare palettes data
-    rows.forEach(async (row) => {
-      const columns = row.split(',');
-      const image = columns[0].trim();
-      const style = columns[STYLE_COL].trim();
-      // populates the styles table and caches the id
-      const style_id = await fetchIdFromTable('style', style, cache);
-
-      // insert palette data
-      const palette = {
-        image: columns[0].trim(),
-        style: style,
-        colors: columns.slice(COLOR1_COL, COLOR1_COL + COLOR_COUNT).map(color => color.trim()),
-        filters: columns.slice(COLOR1_COL + COLOR_COUNT).map(filter => filter.trim())
-      };
-
-      const res = sql`
-        INSERT INTO palettes (image, style_id, color1, color2, color3, color4, color5, color6, color7)
-        VALUES (${palette.image}, ${style_id}, ${palette.colors[0]}, ${palette.colors[1]}, ${palette.colors[2]}, ${palette.colors[3]}, ${palette.colors[4]}, ${palette.colors[5]}, ${palette.colors[6]})
-        RETURNING palette_id
-      `;
-      
-    });
-
-    // Populate styles and colors tables
-    await Promise.all(Array.from(styles).map(style => {
-      return sql`INSERT INTO styles (style_name) VALUES (${style})`;
-    }));
-
-    await Promise.all(Array.from(colors).sort().map(color => {
-      return sql`INSERT INTO colors (color_name) VALUES (${color})`;
-    }));
-
-    // Populate palettes table
-    // Note: This part of the code needs to be adapted based on how you plan to store and reference styles and colors
-    for (const palette of palettesData) {
-      const { image, style, colors } = palette;
-      const styleId = LocalCache.styles[style];
-      // Insert palette data (assuming color columns are named color1, color2, etc.)
-      const paletteId = (await sql`INSERT INTO palettes (image, style_id, color1, color2, color3, color4, color5, color6, color7) VALUES (${image}, ${styleId}, ${colors[0]}, ${colors[1]}, ${colors[2]}, ${colors[3]}, ${colors[4]}, ${colors[5]}, ${colors[6]}) RETURNING palette_id`).rows[0].palette_id;
-
-      // Populate palettes_colors table
-      // This assumes you have a mechanism to map color names to color_ids
-      colors.forEach(async colorName => {
-        const colorId = LocalCache.colors[colorName];
-        await sql`INSERT INTO palettes_colors (palette_id, color_id) VALUES (${paletteId}, ${colorId})`;
-      });
     }
   } catch (err) {
     console.error(err);
@@ -213,19 +197,26 @@ export async function populateTablesFromCSV(path: string) {
   }
 }
 
-// Start of script
-// console.log('Creating tables...');
-// createTables().then(() => {
-//   console.log('Populating tables from CSV...');
-//   populateTablesFromCSV('path/to/your/csvfile.csv');
-// });
+async function clearTables() {
+  try {
+    await sql`DELETE FROM palettes_filters;`
+    await sql`DELETE FROM palettes;`
+    await sql`DELETE FROM styles;`
+    await sql`DELETE FROM filters;`
+  } catch (err) {
+    console.error(err);
+    throw new Error('Error clearing tables');
+  }
+}
 
 // start of script
 async function main() {
+  console.log('Clearing tables...');
+  clearTables()
   console.log('Creating tables...');
   await createTables();
   console.log('Populating tables from CSV...');
-  await populateTablesFromCSV('path/to/your/csvfile.csv');
+  await populateTablesFromCSV('./scripts/parsed_palettes_sm.csv');
 
 }
 
