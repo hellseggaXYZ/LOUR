226519857682ce022848202aa461fa91aa1f9663
szgbo <shaoboz627@gmail.com>
2023-12-26 21:17:45 +0800

    why can't i commit

--- src/components/ColorButton.tsx
+++ src/components/ColorButton.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import styles from './ColorButton.module.css';
 
 function ColorButton({ colorName, selected }: { colorName: string, selected: boolean }) {
-  const className = selected ? `${styles.colorButton} ${styles.selected}` : styles.colorButton;
+  const className = `${styles.colorButton} 
+  ${selected ? styles.selected : ''} 
+  ${colorName === 'white' ? styles.whiteBorder : ''}`;
 
   return (
     <div className={className} style={{ backgroundColor: colorName }} />
