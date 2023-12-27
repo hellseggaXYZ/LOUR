fed367a988d11e23ff196fe879a639c76afa2d6d
Marco Wang <marcowang01@gmail.com>
2023-12-27 13:35:46 +0800

    add transitions for icons

--- src/components/PalleteGrid.tsx
+++ src/components/PalleteGrid.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styles from './PalleteGrid.module.css';
-import { patterns, grids, isLight } from '@/util/colorPatterns';
+import { grids, isLight } from '@/util/colorPatterns';
 import { CopyIcon } from '@/svg/copy';
 import { CheckIcon } from '@/svg/check';
 
@@ -9,65 +9,59 @@ const PalleteGrid = ({ colors }: { colors: string[] }) => {
     return <p>Invalid color array</p>;
   }
 
-
   const grid = grids[0];
-
-  const [copied, setCopied] = useState(Array(grid.cells.length).fill(false)); // State to track copied status of each cell
+  const [copied, setCopied] = useState(Array(grid.cells.length).fill(false));
   const [isMobile, setIsMobile] = useState(false); // State to track if the screen is mobile
-
   useEffect(() => {
     // check if the screen is mobile
     if (window.innerWidth <= 768) {
       setIsMobile(true);
     }
-
   }, []);
-
 
   const copyToClipboard = (hex: string, index: number) => {
     navigator.clipboard.writeText(hex).then(() => {
-      // Set the copied state to true for the cell
-      setCopied(copied.map((copied, i) => i === index ? true : copied));
-
-      // Set a timeout to change the icon back to CopyIcon after 2 seconds
+      setCopied(copied.map((c, i) => i === index ? true : c));
       setTimeout(() => {
-        setCopied(copied.map((copied, i) => i === index ? false : copied));
-      }, 2000); // 2000 milliseconds = 2 seconds
+        setCopied(copied.map((c, i) => i === index ? false : c));
+      }, 2000);
     });
   }
   
 
   return (
-    <div style={{
-      width: `${grid.width * 7.25}vh`,
-      height: `${grid.height * 7.25}vh`,
-      position: 'relative',
-    }}>
+    <div style={{ width: `${grid.width * 7.25}vh`, height: `${grid.height * 7.25}vh`, position: 'relative' }}>
       {grid.cells.map((cell, index) => {
-        const width = `${cell.w * 7.25}vh`;
-        const height = `${cell.h * 7.25}vh`;
+
         const textColor = isLight(colors[index]) ? '#333' : '#CCC';
 
         return (
-          <div 
+          <div
             key={`cell-${index}`}
             className={styles.cell}
-            style={{ 
+            style={{
               backgroundColor: colors[index],
               position: 'absolute',
               left: `${cell.x * 7.25}vh`,
               top: `${cell.y * 7.25}vh`,
-              width: width, 
-              height: height,
-            }} 
+              width: `${cell.w * 7.25}vh`,
+              height: `${cell.h * 7.25}vh`,
+            }}
             onClick={() => copyToClipboard(colors[index], index)}
           >
-            <div className={styles.copyIcon} style={{ color: textColor }}>
-              {isMobile ? 
-                <CopyIcon /> : 
-                copied[index] ? <CheckIcon /> : <CopyIcon />
-              }
-            </div>
+            {isMobile ?
+              (
+                <div className={styles.copyIcon} style={{ color: textColor }}>
+                  <CopyIcon />
+                </div>
+              ) : (
+                <div className={`${styles.copyIcon} ${copied[index] ? styles.copied : ''}`} style={{ color: textColor }}>
+                  <CopyIcon style={{ opacity: copied[index] ? 0 : 1 }} />
+                  <CheckIcon style={{ opacity: copied[index] ? 1 : 0 }} />
+                </div>
+              )
+            }
+            
             <div className={styles.hexText} style={{ color: textColor }}>
               {colors[index]}
             </div>
